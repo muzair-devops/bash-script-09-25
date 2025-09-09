7bb00c7b721fb3ca3ec608fdc3fef3b8c01cc9c5
Uzair <uzair_live@proton.me>
2025-09-09 03:27:11 +0000

    Add script file

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the current date and time in YYYY-MM-DD HH:MM:SS format
+current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Print the formatted date and time
+echo "Current Date and Time: $current_datetime"
